a2297c9b1f4e8fbcdd0f9bdf0ee5d20ba23c9d9e
boris lazarevic <boris.lazarevic@htecgroup.com>
2023-04-20 09:05:09 +0200

    [ feature: issue-24 ] Remove reducePath api because it is default behavior

--- packages/client/src/redux-toolkit/api.ts
+++ packages/client/src/redux-toolkit/api.ts
@@ -3,7 +3,6 @@ import { createApi } from '@reduxjs/toolkit/query/react';
 import { UserList, trpc } from '../lib/trpc';
 
 export const api = createApi({
-  reducerPath: 'api',
   baseQuery: (trpcResult: Promise<unknown>) => trpcResult.then((data) => ({ data })).catch((error) => ({ error })),
   endpoints: (builder) => ({
     getUsers: builder.query<UserList, void>({
