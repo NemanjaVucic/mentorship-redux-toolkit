182aa8e914b1f35693604c4c73538d31d98c31e1
Nemanja Vucic <nemanja.vucic@htecgroup.com>
2023-05-26 10:40:40 +0200

    [feat : issue-31 ] user-client to users

--- apps/user-client/src/pages/home-page/HomePage.styled.tsx
+++
@@ -1,8 +0,0 @@
-import { space } from '@mentorship/shared/ui';
-import styled from 'styled-components';
-
-export const CardWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  gap: ${space.x5};
-`;
