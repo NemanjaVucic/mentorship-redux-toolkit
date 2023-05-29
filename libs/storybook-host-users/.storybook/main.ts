00bf1966a12d55faff1c50631527836b73d11038
Nemanja Vucic <nemanja.vucic@htecgroup.com>
2023-05-29 16:39:14 +0200

    [ fix : issue-31 ] delete unnecessary properties

--- libs/storybook-host-users/.storybook/main.ts
+++ libs/storybook-host-users/.storybook/main.ts
@@ -5,14 +5,11 @@ import rootMain from '../../../.storybook/main';
 const config: StorybookConfig = {
   ...rootMain,
   core: {
+    ...rootMain.core,
     builder: '@storybook/builder-vite',
   },
   stories: ['../../users/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [...(rootMain.addons ?? []), '@nx/react/plugins/storybook'],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
 };
 
 export default config;
