d1f346b93ed52f316e0cb78dcf9f851635d39921
Nemanja Vucic <nemanja.vucic@htecgroup.com>
2023-06-02 11:24:35 +0200

    [ fix: issue-37 ] removed unused types

--- libs/users/shared/schema/src/delete-user/schema.ts
+++ libs/users/shared/schema/src/delete-user/schema.ts
@@ -1,5 +1,3 @@
 import { z } from 'zod';
 
 export const deleteUserSchema = z.object({ id: z.number() });
-
-export type DeleteUserRequest = z.infer<typeof deleteUserSchema>;
--- libs/users/shared/schema/src/update-user/schema.ts
+++ libs/users/shared/schema/src/update-user/schema.ts
@@ -3,5 +3,3 @@ import { z } from 'zod';
 import { createUserSchema } from '../create-user';
 
 export const updateUserSchema = z.object({ data: createUserSchema.partial() }).merge(z.object({ id: z.number() }));
-
-export type UpdateUserRequest = z.infer<typeof updateUserSchema>;
